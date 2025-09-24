48c5e5d496350107b3b0e0fbb93e0e25e97484e0
Lee <111371642+gracelee087@users.noreply.github.com>
2025-09-24 17:16:51 +0200

    Delete code directory

--- code/2-8.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-# LLM 또는 CHAT MODEL이 있음!! 
-
-from langchain.chat_models import ChatOpenAI
-chat_model = ChatOpenAI()
-result = chat_model.predict("hi")
-print(result)
--- code/5-3.py
+++
@@ -1,6 +0,0 @@
-from langchain.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("dbs.pdf")
-pages = loader.load_and_split()
-
-print(pages[5])
